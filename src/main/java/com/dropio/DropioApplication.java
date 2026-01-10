77dfc7b181a28e9616e1f16b102d8def73c6828a
orhan <orhantrkmn749@gmail.com>
2026-01-11 02:00:07 +0300

    Initial commit

---
+++ src/main/java/com/dropio/DropioApplication.java
@@ -0,0 +1,13 @@
+package com.dropio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropioApplication.class, args);
+	}
+
+}
